75424d95c4423cdcd449f9e3d3cbba6370dc49fc
Hmm465GamingYT <Hmm465GamingYT@users.noreply.github.com>
2018-06-23 18:41:00 -0400

    Add files via upload

---
+++ perm_ban.lua
@@ -0,0 +1,10 @@
+pban = {} --inside the save file for IY
+pban = readfile("IY.txt")
+for i,asd in pairs(game.Players:GetChildren()) do
+if pban[asd.UserId] then
+banfunction(asd)
+end
+game.Players.OnPlayerAdded:connect(function(asdd)
+if pban[asdd.UserId] then
+banfunction(asdd)
+end)
